1854e7b63815e8ed4eba1b5c2fd99623121c3b85
Satwik Mishra <150122188+satwikmishra11@users.noreply.github.com>
2026-07-13 19:05:55 +0530

    refactor: remove unused DTO classes and add Lombok annotations to WorkerHeartbeat

--- controller/src/main/java/com/loadtest/model/WorkerHeartbeat.java
+++ controller/src/main/java/com/loadtest/model/WorkerHeartbeat.java
@@ -2,10 +2,14 @@ package com.loadtest.model;
 
 import lombok.Data;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 import java.time.Instant;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class WorkerHeartbeat {
     private String workerId;
     private Instant timestamp;
